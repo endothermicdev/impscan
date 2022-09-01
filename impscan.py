098181ed25d3b6737f811779608b92663f0694b1
Alex Myers <alex@endothermic.dev>
2022-09-01 15:03:10 -0500

    cleanup: found my linter!

--- impscan.py
+++ impscan.py
@@ -4,33 +4,34 @@ from enum import Enum
 
 plugin = Plugin()
 
-#Still experimental, not interop tested/approved.
-pending_features = {"OPTION_WILL_FUND_FOR_FOOD":30,
-                    "OPTION_INV_GOSSIP":34,
-                    "OPTION_SCHNORR_GOSSIP":38,
-                    "KEYSEND":54,
-                    "TRUSTED_SWAP_IN_PROVIDER":142}
-#Established features from BOLT7
-est_features = {"OPTION_DATA_LOSS_PROTECT":0,
-                "INITIAL_ROUTING_SYNC":2,
-                "OPTION_UPFRONT_SHUTDOWN_SCRIPT":4,
-                "OPT_GOSSIP_QUERIES":6,
-                "VAR_ONION_OPTIN":8,
-                "GOSSIP_QUERIES_EX":10,
-                "OPTION_STATIC_REMOTEKEY":12,
-                "PAYMENT_SECRET":14,
-                "BASIC_MPP":16,
-                "OPTION_SUPPORT_LARGE_CHANNEL":18,
-                "OPTION_ANCHOR_OUTPUTS":20,
-                "OPTION_ANCHORS_ZERO_FEE_HTLC_TX":22,
-                "OPTION_SHUTDOWN_ANYSEGWIT":26,
-                "OPT_DUAL_FUND":28,
-                "OPTION_CHANNEL_TYPE":44,
-                "OPTION_SCID_ALIAS":46,
-                "OPTION_PAYMENT_METADATA":48,
-                "OPT_ZEROCONF":50}
+# Still experimental, not interop tested/approved.
+pending_features = {"OPTION_WILL_FUND_FOR_FOOD": 30,
+                    "OPTION_INV_GOSSIP": 34,
+                    "OPTION_SCHNORR_GOSSIP": 38,
+                    "KEYSEND": 54,
+                    "TRUSTED_SWAP_IN_PROVIDER": 142}
+# Established features from BOLT7
+est_features = {"OPTION_DATA_LOSS_PROTECT": 0,
+                "INITIAL_ROUTING_SYNC": 2,
+                "OPTION_UPFRONT_SHUTDOWN_SCRIPT": 4,
+                "OPT_GOSSIP_QUERIES": 6,
+                "VAR_ONION_OPTIN": 8,
+                "GOSSIP_QUERIES_EX": 10,
+                "OPTION_STATIC_REMOTEKEY": 12,
+                "PAYMENT_SECRET": 14,
+                "BASIC_MPP": 16,
+                "OPTION_SUPPORT_LARGE_CHANNEL": 18,
+                "OPTION_ANCHOR_OUTPUTS": 20,
+                "OPTION_ANCHORS_ZERO_FEE_HTLC_TX": 22,
+                "OPTION_SHUTDOWN_ANYSEGWIT": 26,
+                "OPT_DUAL_FUND": 28,
+                "OPTION_CHANNEL_TYPE": 44,
+                "OPTION_SCID_ALIAS": 46,
+                "OPTION_PAYMENT_METADATA": 48,
+                "OPT_ZEROCONF": 50}
 
 possFeatures = {**pending_features, **est_features}
+
 
 class feature(Enum):
     MANDATORY = 'mandatory'
@@ -39,6 +40,7 @@ class feature(Enum):
     NOT_OPTIONAL = 'not optional'
     NOT_SET = 'not set'
 
+
 class heuristic(object):
     def __init__(self, implementation_name, **features):
         self.name = implementation_name
@@ -46,9 +48,9 @@ class heuristic(object):
 
     def testFeature(self, features, bitnumber):
         """Test for mandatory or optional positions."""
-        return((features&(1<<bitnumber)) != 0)
+        return ((features & (1 << bitnumber)) != 0)
 
-    def test(self,features):
+    def test(self, features):
         """Test a features bitfield against this heuristic."""
         assert isinstance(features, int)
         for fname, fvalue in self.features.items():
@@ -66,42 +68,44 @@ class heuristic(object):
                     return False
             if fvalue == feature.NOT_SET:
                 if self.testFeature(features, possFeatures[fname]) or\
-                self.testFeature(features, possFeatures[fname]+1):
+                   self.testFeature(features, possFeatures[fname]+1):
                     return False
         return True
 
 
 CLN_EXP = heuristic("CLN Experimental",
-                    OPT_DUAL_FUND = feature.OPTIONAL,
-                    GOSSIP_QUERIES_EX = feature.OPTIONAL)
+                    OPT_DUAL_FUND=feature.OPTIONAL,
+                    GOSSIP_QUERIES_EX=feature.OPTIONAL)
 
 Eclair = heuristic("Eclair",
-                   OPTION_SUPPORT_LARGE_CHANNEL = feature.OPTIONAL,
-                   OPTION_ANCHORS_ZERO_FEE_HTLC_TX = feature.OPTIONAL,
-                   OPTION_DATA_LOSS_PROTECT = feature.OPTIONAL)
+                   OPTION_SUPPORT_LARGE_CHANNEL=feature.OPTIONAL,
+                   OPTION_ANCHORS_ZERO_FEE_HTLC_TX=feature.OPTIONAL,
+                   OPTION_DATA_LOSS_PROTECT=feature.OPTIONAL)
 
-LND = heuristic("LND", OPTION_DATA_LOSS_PROTECT = feature.MANDATORY)
+LND = heuristic("LND", OPTION_DATA_LOSS_PROTECT=feature.MANDATORY)
 
 CLN = heuristic("CLN",
-                OPTION_DATA_LOSS_PROTECT = feature.OPTIONAL,
-                OPTION_STATIC_REMOTEKEY = feature.NOT_MANDATORY,
-                GOSSIP_QUERIES_EX = feature.OPTIONAL)
+                OPTION_DATA_LOSS_PROTECT=feature.OPTIONAL,
+                OPTION_STATIC_REMOTEKEY=feature.NOT_MANDATORY,
+                GOSSIP_QUERIES_EX=feature.OPTIONAL)
 
 LDK = heuristic("LDK",
-                VAR_ONION_OPTIN = feature.MANDATORY)
+                VAR_ONION_OPTIN=feature.MANDATORY)
 
 Unknown = heuristic("2200",
-                    VAR_ONION_OPTIN = feature.OPTIONAL,
-                    OPTION_STATIC_REMOTEKEY = feature.OPTIONAL)
+                    VAR_ONION_OPTIN=feature.OPTIONAL,
+                    OPTION_STATIC_REMOTEKEY=feature.OPTIONAL)
 
 
 # heuristics should be ordered from most unique/restrictive features to least
 all_heuristics = [CLN_EXP, Eclair, LND, CLN, LDK, Unknown]
 
+
 def testFeature(features, bitnumber):
     """Test for mandatory or optional positions."""
     assert isinstance(features, int)
-    return((features&(1<<bitnumber)) != 0)
+    return ((features & (1 << bitnumber)) != 0)
+
 
 def identifyFingerprint(feat):
     assert isinstance(feat, int)
@@ -109,6 +113,7 @@ def identifyFingerprint(feat):
         if h.test(feat):
             return h.name
     return ("indef")
+
 
 def unknownFeatures(features):
     known_features = []
@@ -121,23 +126,25 @@ def unknownFeatures(features):
             unknown.append(x)
     return unknown
 
+
 def decodeFeatures(features):
     """pass a feature bit string (HEX) and return a human readable output."""
     assert isinstance(features, str)
     try:
         f = int(features, 16)
     except:
-        return(["feature bit decode failed. (hex encoding required)"])
+        return (["feature bit decode failed. (hex encoding required)"])
     result = {}
     for k, v in possFeatures.items():
         if testFeature(f, possFeatures[k]+1):
-            result.update({"{:<4} {}".format(v+1,k):"optional"})
+            result.update({"{:<4} {}".format(v+1, k): "optional"})
         elif testFeature(f, possFeatures[k]):
-            result.update({"{:<4} {}".format(v,k):"mandatory"})
+            result.update({"{:<4} {}".format(v, k): "mandatory"})
     uf = unknownFeatures(f)
     if (len(uf) > 0):
-        result.update({"Unknown features":uf})
+        result.update({"Unknown features": uf})
     return result
+
 
 def queryNodeFeatures(nodeid):
     if (len(str(nodeid)) != 66):
@@ -147,32 +154,36 @@ def queryNodeFeatures(nodeid):
     except RpcError as e:
         if ('code', -32602) in e.error.items():
             return ("Nodeid not found. Invalid token")
-        return("Error encountered during call to listnodes: {}".format(e))
+        return "Error encountered during call to listnodes: {}".format(e)
     node = query['nodes'][0]
     return node["features"]
 
+
 def singleNodeDecode(nodeid):
     return decodeFeatures(queryNodeFeatures(nodeid))
+
 
 def testNodes(test_items):
     """feed a json dict of node_id:heuristic pairs and this will fingerprint
     the nodes and validate that they match the desired heuristic. A list of
     nodes from all implementations will validate that all the heuristics
     remain effective."""
-    if not isinstance(test_items,dict):
-        return """test argument requires a json dict of format '{"node_id":"heuristic"}'"""
+    if not isinstance(test_items, dict):
+        return "test argument requires a json dict of format \
+'{\"node_id\":\"heuristic\"}'"
     answer = []
     for node, h in test_items.items():
         f = queryNodeFeatures(node)
         if "Error" in f:
             return f
-        fingerprint = identifyFingerprint(int(f,16))
-        answer.append({"node_id":node,
-                       "features":f,
-                       "test heuristic":h,
-                       "fingerprint":fingerprint,
-                       "status": fingerprint==h})
+        fingerprint = identifyFingerprint(int(f, 16))
+        answer.append({"node_id": node,
+                       "features": f,
+                       "test heuristic": h,
+                       "fingerprint": fingerprint,
+                       "status": fingerprint == h})
     return answer
+
 
 def fullScan():
     """Fingerprint all nodes on the network using all heuristics."""
@@ -180,8 +191,8 @@ def fullScan():
     for h in all_heuristics:
         for f in h.features.keys():
             if f not in possFeatures.keys():
-                return({"error":"{} not a possible feature "
-                        "({} heuristic)".format(f, h.name)})
+                return {"error": "{} not a possible feature "
+                        "({} heuristic)".format(f, h.name)}
     plugin.log("impscan calling listnodes via rpc")
     nodes = plugin.rpc.listnodes()['nodes']
     s = "impscan: listnodes returned {} items".format(len(nodes))
@@ -189,18 +200,18 @@ def fullScan():
     c = 0
     result = {}
     for h in all_heuristics:
-        result.update({h.name:0})
-    result.update({"indef":0,"no features":0})
+        result.update({h.name: 0})
+    result.update({"indef": 0, "no features": 0})
     indefs = []
     for n in nodes:
-        c+=1
+        c += 1
         if ("features" not in n):
             result["no features"] += 1
             continue
-        if(n["features"] == None or n["features"] == ''):
+        if (n["features"] is None or n["features"] == ''):
             result["no features"] += 1
             continue
-        r = identifyFingerprint(int(n["features"],16))
+        r = identifyFingerprint(int(n["features"], 16))
         if r == "indef":
             indefs.append(n)
         result[r] += 1
@@ -216,20 +227,21 @@ def impscan(plugin, **kwargs):
     """Estimate breakdown of various lightning implementations on the network.
     This relies on the listnodes command and feature bits. Work in progress."""
     for k in kwargs.keys():
-        if k not in ["node","features","test"]:
-            return(["unrecognized keyword '{}'".format(k)])
+        if k not in ["node", "features", "test"]:
+            return ["unrecognized keyword '{}'".format(k)]
     if ("node" in kwargs.keys()):
         return singleNodeDecode(kwargs["node"])
     if ("features" in kwargs.keys()):
         return decodeFeatures(kwargs["features"])
     if ("test" in kwargs.keys()):
         return testNodes(kwargs["test"])
-    #Run analysis on all network nodes
+    # Run analysis on all network nodes
     return fullScan()
 
 
 @plugin.init()
 def init(options, configuration, plugin, **kwargs):
     plugin.log("Plugin impscan.py initialized")
+
 
 plugin.run()
